3d3bee1d28825f02ff47cc508561b27de079cd75
I M E S H A <163811096+ImeshaNadeeshani@users.noreply.github.com>
2025-04-27 14:52:37 +0530

    Add files via upload

---
+++ skillShare/skillShare/src/main/java/com/skillShare/skillShare/repository/FeedbackRepository.java
@@ -0,0 +1,10 @@
+package com.skillShare.skillShare.repository;
+
+
+import com.skillShare.skillShare.entity.Feedback;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface FeedbackRepository extends MongoRepository<Feedback, String> {
+    // You can define custom MongoDB queries here if needed
+}
+
